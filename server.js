17d1d7f0c22959c336a60ae0ee7a383fd93cb168
Deepak Unni <deepak.unni3@gmail.com>
2016-12-13 11:55:47 -0600

    The right way of routing

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require("express");
-var app     = express();
+
 var path    = require("path");
 var passport = require("passport");
 var passport_http = require("passport-http");
 var port = 4444
 
-//app.use(express.static(path.join(__dirname, 'resources')));
-app.use('/three-bee-project/resources', express.static('resources'));
-app.use('/three-bee-project/datasets', express.static('datasets'));
+var router = express.Router();
+var app = express();
+// router.get("/", function(req, res) {
+//   console.log("request to router");
+//   res.send({ message: "Whaddup!"});
+// });
+
+app.use("/three-bee-project", router);
+router.use('/resources', express.static('resources'));
+router.use('/datasets', express.static('datasets'));
 
 passport.use(
   new passport_http.BasicStrategy(
     function(username, password, done) {
       console.log("Inside middleware");
-      if (username.valueOf() === 'test1' && password.valueOf() === 'test1') {
+      if (username.valueOf() === 'test' && password.valueOf() === 'test') {
         console.log("VALID credentials");
         return done(null, true);
       }
@@ -25,25 +32,25 @@ passport.use(
   )
 );
 
-app.get('/',
+router.get('/',
   passport.authenticate('basic', { session: false }),
   function(req, res) {
-    console.log("Redirecting / to /index.html after successful authentication");
-    res.redirect('/three-bee-project');
-});
-
-app.get('/three-bee-project',
-  passport.authenticate('basic', { session: false }),
-  function(req, res) {
-    console.log("Redirecting / to /index.html after successful authentication");
+    console.log("Redirecting / to /three-bee-project/index.html after successful authentication");
     res.redirect('/three-bee-project/index.html');
 });
 
-app.get('/three-bee-project/index.html',
+router.get('/index.html',
   passport.authenticate('basic', { session: false }),
   function(req, res) {
     console.log("Serving /index.html after successful authentication");
     res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/',
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+    console.log("Redirecting / to /three-bee-project/index.html after successful authentication");
+    res.redirect('/three-bee-project/index.html');
 });
 
 app.use(function (req, res, next) {
