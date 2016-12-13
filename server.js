93e98e4eb75070a0fba74ec32d383d576eea4c60
Deepak Unni <deepak.unni3@gmail.com>
2016-12-13 09:27:59 -0600

    enable serving static files, routing and handling 404s

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ var path    = require("path");
 var passport = require("passport");
 var passport_http = require("passport-http");
 
+
+//app.use(express.static(path.join(__dirname, 'resources')));
+app.use('/resources', express.static('resources'));
+
 passport.use(new passport_http.BasicStrategy(
   function(username, password, done) {
-    // User.findOne({ username: username }, function (err, user) {
-    //   if (err) { return done(err); }
-    //   if (!user) { return done(null, false); }
-    //   if (!user.validPassword(password)) { return done(null, false); }
-    //   return done(null, user);
-    // });
-    if (username.valueOf() === 'test123' && password.valueOf() === 'test123') {
+    console.log("Inside middleware");
+    if (username.valueOf() === 'test1' && password.valueOf() === 'test1') {
+      console.log("VALID credentials");
         return done(null, true);
     }
     else {
+      console.log("INVALID credentials");
       return done(null, false);
     }
   }
@@ -24,16 +25,22 @@ passport.use(new passport_http.BasicStrategy(
 app.get('/',
   passport.authenticate('basic', { session: false }),
   function(req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
-  });
+    console.log("Redirecting / to /index.html after successful authentication");
+    res.redirect('/index.html');
+});
 
-// app.get('/about',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
-// });
-//
-// app.get('/sitemap',function(req,res){
-//   res.sendFile(path.join(__dirname+'/sitemap.html'));
-// });
+app.get('/index.html',
+  passport.authenticate('basic', { session: false }),
+  function(req, res) {
+    console.log("Serving /index.html after successful authentication");
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use(function (req, res, next) {
+  console.log(req.url);
+  console.log("Servinc 404.html");
+  res.status(404).sendFile(path.join(__dirname+'/404.html'));
+})
 
 app.listen(4444);
 
